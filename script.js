66c6311d7c69a385d14e6da3b66e4280f903a76c
Youngmook Lim <youngmookk@gmail.com>
2022-05-23 20:21:19 +0900

    Add minor styling

--- script.js
+++ script.js
@@ -1,32 +1,23 @@
 "use strict";
 
-const calcAdd = function (x, y) {
-  return x + y;
+const btnAdd = document.querySelector(".btn--add");
+const btnSub = document.querySelector(".btn--sub");
+const btnMul = document.querySelector(".btn--mul");
+const btnDiv = document.querySelector(".btn--div");
+
+const calcAdd = (x, y) => x + y;
+const calcSub = (x, y) => x - y;
+const calcMul = (x, y) => x * y;
+const calcDiv = (x, y) =>
+  Math.round((x / y + Number.EPSILON) * 100000) / 100000;
+const operate = (op, x, y) => {
+  if (op === "+") return calcAdd(x, y);
+  if (op === "-") return calcSub(x, y);
+  if (op === "×") return calcMul(x, y);
+  if (op === "÷") return calcDiv(x, y);
 };
 
-const calcSub = function (x, y) {
-  return x - y;
-};
-
-const calcMul = function (x, y) {
-  return x * y;
-};
-
-const calcDiv = function (x, y) {
-  return Math.round((x / y + Number.EPSILON) * 100000) / 100000;
-};
-
-const operate = function (operator, x, y) {
-  if (operator === "+") {
-    return calcAdd(x, y);
-  }
-  if (operator === "-") {
-    return calcSub(x, y);
-  }
-  if (operator === "*") {
-    return calcMul(x, y);
-  }
-  if (operator === "/") {
-    return calcDiv(x, y);
-  }
-};
+btnDiv.addEventListener("click", function (e) {
+  console.log(e.target.textContent);
+  console.log(operate(e.target.textContent, 5, 4));
+});
