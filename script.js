a698c097cf6b913bac367614ba73a7ff3fecb857
Youngmook Lim <youngmookk@gmail.com>
2022-05-23 19:54:40 +0900

    Set up html and css for calculator

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 "use strict";
+
+const calcAdd = function (x, y) {
+  return x + y;
+};
+
+const calcSub = function (x, y) {
+  return x - y;
+};
+
+const calcMul = function (x, y) {
+  return x * y;
+};
+
+const calcDiv = function (x, y) {
+  return Math.round((x / y + Number.EPSILON) * 100000) / 100000;
+};
+
+const operate = function (operator, x, y) {
+  if (operator === "+") {
+    return calcAdd(x, y);
+  }
+  if (operator === "-") {
+    return calcSub(x, y);
+  }
+  if (operator === "*") {
+    return calcMul(x, y);
+  }
+  if (operator === "/") {
+    return calcDiv(x, y);
+  }
+};
