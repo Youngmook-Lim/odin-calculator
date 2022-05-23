e6baed79aa6ec77f0fc3c8a927eedb168ed27877
Youngmook Lim <youngmookk@gmail.com>
2022-05-23 23:41:13 +0900

    Add advanced functionalities to calculations

--- script.js
+++ script.js
@@ -10,6 +10,13 @@ const btnDiv = document.querySelector(".btn--div");
 const btnNum = document.querySelector(".btn--num");
 const screenCalc = document.querySelector(".screen-calc");
 const screenRes = document.querySelector(".screen-res");
+let activeNum = "";
+
+const operation = {
+  op: "",
+  values: [],
+  answer: "",
+};
 
 // Operator Functions //
 const calcAdd = (x, y) => x + y;
@@ -24,12 +31,32 @@ const operate = (op, x, y) => {
   if (op === "÷") return calcDiv(x, y);
 };
 
-const checkLastChar = function () {
+const deleteChar = function () {
+  if (!screenCalc.textContent || screenRes.textContent) return;
   const screenCalcTemp = screenCalc.textContent.replace(/ /g, "");
 
   if (!isNaN(+screenCalcTemp[screenCalcTemp.length - 1])) {
+    if (!activeNum) {
+      activeNum = operation.values[0].toString();
+      operation.values.shift();
+    }
+    activeNum = activeNum.slice(0, -1);
     screenCalc.textContent = screenCalc.textContent.slice(0, -1);
-  } else screenCalc.textContent = screenCalc.textContent.slice(0, -3);
+  } else {
+    operation.op = "";
+    screenCalc.textContent = screenCalc.textContent.slice(0, -3);
+  }
+};
+
+const clearOperation = function () {
+  operation.op = "";
+  operation.values = [];
+  operation.answer = "";
+};
+
+const clearText = function () {
+  screenCalc.textContent = "";
+  screenRes.textContent = "";
 };
 
 btns.addEventListener("click", function (e) {
@@ -37,19 +64,52 @@ btns.addEventListener("click", function (e) {
   if (!btn) return;
   const btnText = btn.textContent;
 
-  if (!isNaN(+btnText)) {
+  if (!isNaN(+btnText) || btnText === ".") {
+    if (btnText === "." && activeNum.includes(".")) return;
     screenCalc.textContent += btnText;
+    activeNum += btnText;
   } else if (btnText === "DELETE") {
-    checkLastChar();
+    deleteChar();
   } else if (btnText === "CLEAR") {
-    screenCalc.textContent = "";
-    screenResCalc.textContentt = "";
-  } else screenCalc.textContent += ` ${btnText} `;
+    clearText();
+    clearOperation();
+  } else if (btnText === "=") {
+    if (!operation.op) return;
+    if (!activeNum) return;
+    screenCalc.textContent += ` ${btnText} `;
+    operation.values.push(+activeNum);
+    activeNum = "";
+    const answer = operate(
+      operation.op,
+      operation.values[0],
+      operation.values[1]
+    );
+    screenRes.textContent = answer;
+    clearOperation();
+    operation.answer = answer;
+  } else {
+    if (!operation.answer) {
+      screenCalc.textContent += ` ${btnText} `;
+      operation.values.push(+activeNum);
+      activeNum = "";
+      operation.op = btnText;
+    } else {
+      clearText();
+      const temp = operation.answer;
+      screenCalc.textContent += `${temp} ${btnText} `;
+      operation.values.push(temp);
+      activeNum = "";
+      operation.op = btnText;
+    }
+  }
+
+  console.log(operation);
+  console.log(activeNum);
 });
 
 const init = function () {
-  screenCalc.textContent = "";
-  screenRes.textContent = "";
+  clearText();
+  clearOperation();
 };
 
 init();
