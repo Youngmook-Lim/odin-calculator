81ceebc78726a18d65bee22f1a74619eb36b2ebf
Youngmook Lim <youngmookk@gmail.com>
2022-05-23 22:41:50 +0900

    Add event listener to buttons and show calculation on screen

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 "use strict";
 
+// QuerySelectors for DOM Elements //
+
+const btns = document.querySelector(".btns");
 const btnAdd = document.querySelector(".btn--add");
 const btnSub = document.querySelector(".btn--sub");
 const btnMul = document.querySelector(".btn--mul");
 const btnDiv = document.querySelector(".btn--div");
+const btnNum = document.querySelector(".btn--num");
+const screenCalc = document.querySelector(".screen-calc");
+const screenRes = document.querySelector(".screen-res");
 
+// Operator Functions //
 const calcAdd = (x, y) => x + y;
 const calcSub = (x, y) => x - y;
 const calcMul = (x, y) => x * y;
@@ -17,7 +24,32 @@ const operate = (op, x, y) => {
   if (op === "÷") return calcDiv(x, y);
 };
 
-btnDiv.addEventListener("click", function (e) {
-  console.log(e.target.textContent);
-  console.log(operate(e.target.textContent, 5, 4));
+const checkLastChar = function () {
+  const screenCalcTemp = screenCalc.textContent.replace(/ /g, "");
+
+  if (!isNaN(+screenCalcTemp[screenCalcTemp.length - 1])) {
+    screenCalc.textContent = screenCalc.textContent.slice(0, -1);
+  } else screenCalc.textContent = screenCalc.textContent.slice(0, -3);
+};
+
+btns.addEventListener("click", function (e) {
+  const btn = e.target.closest(".btn");
+  if (!btn) return;
+  const btnText = btn.textContent;
+
+  if (!isNaN(+btnText)) {
+    screenCalc.textContent += btnText;
+  } else if (btnText === "DELETE") {
+    checkLastChar();
+  } else if (btnText === "CLEAR") {
+    screenCalc.textContent = "";
+    screenResCalc.textContentt = "";
+  } else screenCalc.textContent += ` ${btnText} `;
 });
+
+const init = function () {
+  screenCalc.textContent = "";
+  screenRes.textContent = "";
+};
+
+init();
